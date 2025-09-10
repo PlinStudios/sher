822f6b14341518e55c6e203306591b169a7cd276
PlinStudios <plincompany2@gmail.com>
2025-09-10 20:10:00 -0300

    cd y exit implementado

--- shell.c
+++ shell.c
@@ -2,26 +2,35 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int c_pid;
+char cwd[1024];
 
 int main(int argc, char *argv[]){
     while (1){
-        printf(">");
+        getcwd(cwd, sizeof(cwd));
+        printf("%s>",cwd);
         char *args[3];
         args[0] = (char*)malloc(100);
         args[1] = (char*)malloc(100);
         args[2] = NULL;
         scanf("%s %s",args[0],args[1]);
 
-        c_pid = fork();
-        if (c_pid==0)
-            execvp(args[0],args);
-        else if (c_pid<0)
-            printf("upsi\n");
+        if (strcmp(args[0],"cd") == 0)
+            chdir(args[1]);
+        else if (strcmp(args[0],"exit") == 0)
+            exit(0);
         else{
-            int status;
-            waitpid(c_pid,&status,0);
+            c_pid = fork();
+            if (c_pid==0)
+                execvp(args[0],args);
+            else if (c_pid<0)
+                printf("upsi\n");
+            else{
+                int status;
+                waitpid(c_pid,&status,0);
+            }
         }
     }
 }
