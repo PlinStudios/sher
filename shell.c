4f4b53d40aa2351b81eadb050cd9f734ec304324
PlinStudios <plincompany2@gmail.com>
2025-09-11 17:07:35 -0300

    maeja io con ll amadas a sistema y parsea con strtok

--- shell.c
+++ shell.c
@@ -6,17 +6,28 @@
 
 int c_pid;
 char cwd[1024];
+char buffer[1024];
 
 int main(int argc, char *argv[]){
     while (1){
+        //prompt
         getcwd(cwd, sizeof(cwd));
-        printf("%s>",cwd);
-        char *args[3];
-        args[0] = (char*)malloc(100);
-        args[1] = (char*)malloc(100);
-        args[2] = NULL;
-        scanf("%s %s",args[0],args[1]);
+        //printf("%s> ",cwd);
+        write(1, cwd, strlen(cwd));
+        write(1, "> ", 3);
 
+        //read input
+        ssize_t n = read(0, buffer, 1024);
+        if (n<=0) continue;
+        buffer[n]='\0';
+
+        char *args[3];
+        args[0] = strtok(buffer," \n");
+        args[1] = strtok(NULL," \n");
+        args[2] = NULL;//strtok(NULL," \n");
+
+        //exec command
+        printf(">%s %s %s<",args[0],args[1],args[2]);
         if (strcmp(args[0],"cd") == 0)
             chdir(args[1]);
         else if (strcmp(args[0],"exit") == 0)
