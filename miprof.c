d360ddc8a92865f8b9cf1c46e7473e4829b87b32
PlinStudios <plincompany2@gmail.com>
2025-09-26 12:22:00 -0300

    miprof ejecutar maxtiempo

--- miprof.c
+++ miprof.c
@@ -2,7 +2,11 @@
 #define io_green "\x1b[32m"
 #define io_reset "\x1b[0m"
 
-#define throwusagemsg write(2, io_red"usage: miyprof [ejec|ejecsave file] command args\n"io_reset, sizeof(io_red"usage: myprof [ejec|ejecsave file] command args\n"io_reset))
+#define throwusagemsg write(2, io_red"usage: miyprof [ejec|ejecsave file|ejecutar maxtiempo(ms)] command args\n"io_reset, sizeof(io_red"usage: myprof [ejec|ejecsave file|ejecutar maxtiempo(ms)] command args\n"io_reset))
+
+void timeout_handler(int sig) {
+    _exit(0);
+}
 
 int miprof(int argc, char *argv[]){
     if (argc < 3) {throwusagemsg; return 1;}
@@ -17,6 +21,8 @@ int miprof(int argc, char *argv[]){
         }else if (strcmp(argv[1],"ejecsave") == 0){
             if (argc < 4) {throwusagemsg; exit(0);}
 
+            signal(SIGALRM, timeout_handler);
+
             int fd = open(argv[2], O_WRONLY | O_CREAT | O_APPEND, 0644);
             dup2(fd, STDOUT_FILENO);
 
@@ -26,6 +32,20 @@ int miprof(int argc, char *argv[]){
             }
             write(fd,"\n", 1);
             
+            execvp(argv[3],argv+3);
+            write(2, io_red"non existent command\n"io_reset, sizeof(io_red"non existent command\n"io_reset));
+            exit(0);
+        }else if (strcmp(argv[1],"ejecutar") == 0){
+            if (argc < 4) {throwusagemsg; exit(0);}
+
+            struct itimerval timer;
+            timer.it_value.tv_sec = atoi(argv[2]) / 1000;
+            timer.it_value.tv_usec = (atoi(argv[2]) % 1000) * 1000;
+            timer.it_interval.tv_sec = 0;
+            timer.it_interval.tv_usec = 0;
+
+            setitimer(ITIMER_REAL, &timer, NULL);
+
             execvp(argv[3],argv+3);
             write(2, io_red"non existent command\n"io_reset, sizeof(io_red"non existent command\n"io_reset));
             exit(0);
--- shell.c
+++ shell.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
+#include <sys/time.h>
 
 #include "miprof.c"
 
